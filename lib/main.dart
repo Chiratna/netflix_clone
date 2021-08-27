a1c1d30b7088a6c5d49a11f6255aaab7a624f4e9
Chiratna <prinusuva7876@gmail.com>
2021-08-27 15:42:19 +0530

    updated main

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       initialRoute: HomePageId,
-      title: 'Flutter Demo',
+      //title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
